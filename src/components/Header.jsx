994261b2d15b0e6bee08f7e6a95a3fa639bce9c8
Toma John Musa <tomajohn316@yahoo.com>
2023-02-15 12:03:42 +0100

    Add header and nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,19 @@
+import '../styles/header.css';
+
 const Header = () => (
-  <header>
-    <h1>TOma john</h1>
+  <header className="flex-space">
+    <div className="logo">
+      <a>Kamba</a>
+    </div>
+
+    <nav>
+      <ul className="flex">
+        <li><a href="#">About me</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Skills</a></li>
+        <li><a href="#">Contact me</a></li>
+      </ul>
+    </nav>
   </header>
 );
 
