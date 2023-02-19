e3538aab684e2a3eb228d9ee1aa0f7cf612a5f43
Toma John Musa <tomajohn316@yahoo.com>
2023-02-19 20:44:07 +0100

    Add project pics to markup

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,43 @@
 import '../styles/projects.css';
+import proj1 from '../assets/projects/proj1.png';
+import proj2 from '../assets/projects/proj2.png';
+import proj3 from '../assets/projects/proj3.png';
+import proj4 from '../assets/projects/proj4.png';
 
 const Projects = () => {
   const projects = [
     { 
       id: 1,
       title: 'GDG event landing page',
-      desc: 'loremkamdoemofmocamem aomowa admo oamoe oamcid  oamfijdcmooamc onmainc oanfciodn oamoind omaodc odun omnad onjud onjiadn iwmai',
-      image: 'imc',
-      source: 'micmoe',
-      live: 'imcd',
-    }
+      desc: 'This is an e-vite site to goodgle developers group maiduguri events built with react.',
+      image: proj1,
+      source: 'https://github.com/Kamba56/GDG-Event',
+      live: 'https://gdgmaiduguri.com/',
+    },
+    { 
+      id: 2,
+      title: 'Movie Hub',
+      desc: 'A web application built with react that allows users view their favourite movies and add likes and comments.',
+      image: proj2,
+      source: 'https://github.com/Kamba56/MediaHub',
+      live: 'https://kamba56.github.io/MediaHub/docs/',
+    },
+    { 
+      id: 3,
+      title: 'Math Magicians',
+      desc: 'This is a calculator application built with react. It can perform basic math operations.',
+      image: proj3,
+      source: 'https://github.com/Kamba56/MagicMaths',
+      live: 'https://mathmagicians-kamba.netlify.app/calculator',
+    },
+    { 
+      id: 4,
+      title: 'Crypto Today',
+      desc: 'A web application that shows the current price of crypto currencies.',
+      image: proj4,
+      source: 'https://github.com/Kamba56/FX-Today',
+      live: 'https://lively-kheer-cc474b.netlify.app/',
+    },
   ];
 
   return (
