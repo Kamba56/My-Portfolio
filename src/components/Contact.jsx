752acea164f6ed7cd6b23c2c47e85c14caa77e78
Toma John Musa <tomajohn316@yahoo.com>
2023-02-19 21:22:29 +0100

    Add skills icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,11 +12,10 @@ const Contact = () => {
         <p className='center-text mono'>Reach me at..</p>
 
         <Socials />
-
-        <p className='center-text mono'>Or leave a message and I will get back to you</p>
       </div>
 
       <form action="#" className='container'>
+        <p className='center-text mono'>Or leave a message and I will get back to you</p>
         <div className='container column'>
           <input required type="email" name="email" id="mail" placeholder='Please enter your email' className='input mono' />
           <textarea required name="message" id="message" cols="30" rows="10" placeholder='Type your message here' className='input mono'></textarea>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,16 @@
 import '../styles/skills.css';
 import html from '../assets/icons/html.svg';
 import css from '../assets/icons/css.svg';
+import js from '../assets/icons/javascript.svg';
+import ruby from '../assets/icons/ruby.svg';
+import rails from '../assets/icons/rails.svg';
+import pgsql from '../assets/icons/pgsql.svg';
+import git from '../assets/icons/git.svg';
+import react from '../assets/icons/reactjs.svg';
+import figma from '../assets/icons/figma.svg';
+import redux from '../assets/icons/redux.svg';
+import rspec from '../assets/icons/rspec.svg';
+import jest from '../assets/icons/jest.png';
 
 const SKills = () => {
 
@@ -14,6 +24,56 @@ const SKills = () => {
       id: 2,
       skill: 'CSS',
       img: css
+    },
+    {
+      id: 3,
+      skill: 'JavaScript',
+      img: js
+    },
+    {
+      id: 4,
+      skill: 'React',
+      img: react
+    },
+    {
+      id: 5,
+      skill: 'Redux',
+      img: redux
+    },
+    {
+      id: 6,
+      skill: 'Ruby',
+      img: ruby
+    },
+    {
+      id: 7,
+      skill: 'Ruby on Rails',
+      img: rails
+    },
+    {
+      id: 8,
+      skill: 'Postgres',
+      img: pgsql
+    },
+    {
+      id: 9,
+      skill: 'Figma',
+      img: figma
+    },
+    {
+      id: 10,
+      skill: 'Git',
+      img: git
+    },
+    {
+      id: 11,
+      skill: 'Jest',
+      img: jest
+    },
+    {
+      id: 12,
+      skill: 'Rspec',
+      img: rspec
     }
   ]
 
@@ -23,7 +83,7 @@ const SKills = () => {
 
       <div className='skill-grid content'>
         {skills.map((skill) => (
-          <div className='skill-item' key={skill.id}>
+          <div className='skill-item flex' key={skill.id}>
             <div className='flex'>
               <div className='icon-img'>
                 <img src={skill.img} />
