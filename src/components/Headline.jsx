a35befa2f04995c567aa8604e4cc935a0f8a7e08
Toma John Musa <tomajohn316@yahoo.com>
2023-02-20 22:12:22 +0100

    Add sticky nav and fix grammar errors

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -15,7 +15,7 @@ const Headline = () => (
         </div>
         <h1 className='name mono'>Toma</h1>
         <p className='green-text'>&lt;&gt; A full stack developer &lt;/&gt;</p>
-        <p className='about-me center-text'>Passionate about coding and eager to learn, I am a full-stack developer with 2 years of experience who loves to build great software. Armed with a solid foundation in programming concepts and expertise in responsive design, Javascript, react, ruby, and ruby on rails, I am always striving to write clean, efficient, and maintainable code and am capable of ramping up quickly and efficiently.</p>
+        <p className='about-me center-text'>Passionate about coding and eager to learn, I am a full-stack developer with 2 years of experience who loves to build great software. Armed with a solid foundation in programming concepts and expertise in responsive design, Javascript, React, Ruby, and Ruby on Rails, I am always striving to write clean, efficient, and maintainable code and am capable of ramping up quickly and efficiently.</p>
 
         <Socials />
         <a href='./src/assets/resume.pdf' target='_blank' className='center-text btn2 btn scale'>Get My Resume</a>
